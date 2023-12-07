259c63dd689d9d91a209a85d03ac68b84eeccefe
dishajayaprakash <84782365+dishajayaprakash@users.noreply.github.com>
2023-12-06 23:08:56 -0800

    Updated models.py with resnet

--- models.py
+++ models.py
@@ -109,3 +109,64 @@ class LSTM_base(nn.Module):
         out_events = self.fc_events(lstm_out)
 
         return out_fluent, out_events
+
+class ResNetStutterDetectionModel(nn.Module):
+    def __init__(self, input_dim_mfb, input_dim_f0, num_mfb_features, num_f0_features):
+        super(ResNetStutterDetectionModel, self).__init__()
+
+        # ResNet Blocks for MFB features
+        self.resnet_mfb = ResNetBlock(input_dim_mfb, num_mfb_features)
+
+        # ResNet Blocks for F0 features
+        self.resnet_f0 = ResNetBlock(input_dim_f0, num_f0_features)
+
+        # Calculate the total number of features after flattening the output from both ResNet blocks
+        self.num_combined_features = num_mfb_features * num_mfb_features + num_f0_features * num_f0_features
+
+        # Fully Connected layers for classification
+        self.fc_fluent = nn.Linear(self.num_combined_features, 2)  # Binary classification: Fluent/Dysfluent
+        self.fc_events = nn.Linear(self.num_combined_features, 6)  # 6 dysfluency types
+
+    def forward(self, x_mfb, x_f0):
+        x_mfb = self.resnet_mfb(x_mfb)
+        x_f0 = self.resnet_f0(x_f0)
+
+        # Flatten the outputs
+        x_mfb = x_mfb.view(x_mfb.size(0), -1)
+        x_f0 = x_f0.view(x_f0.size(0), -1)
+
+        # Combine features
+        x_combined = torch.cat((x_mfb, x_f0), dim=1)
+
+        # Classification
+        out_fluent = self.fc_fluent(x_combined)
+        out_events = self.fc_events(x_combined)
+
+        return out_fluent, out_events
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm1d(out_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm1d(out_channels)
+        self.skip = nn.Conv1d(in_channels, out_channels, 1)  # Skip connection
+        self.dropout = nn.Dropout(p=0.5)
+        
+    def forward(self, x):
+        identity = self.skip(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity  # Skip Connection
+        out = self.relu(out)
+        out = self.dropout(out)
+        
+        return out
+model = ResNetStutterDetectionModel(input_dim_mfb=40, input_dim_f0=94, num_mfb_features=200, num_f0_features=200)
